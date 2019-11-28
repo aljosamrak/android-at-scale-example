0590d5e36601d69b401fb2145ffa9e9c469d1979
Aljosa <aljosa.mrak@gmail.com>
2019-11-28 19:55:53 +0100

    Add common library module

---
+++ library/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id(BuildPlugins.androidLibrary)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExtensions)
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(AndroidSdk.compile)
+    buildToolsVersion(AndroidSdk.buildTools)
+
+    defaultConfig {
+        minSdkVersion(AndroidSdk.min)
+        targetSdkVersion(AndroidSdk.target)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(Libraries.kotlinStdLib)
+    implementation(Libraries.appCompat)
+    implementation(Libraries.ktxCore)
+
+    testImplementation (TestLibraries.junit4)
+
+    androidTestImplementation (TestLibraries.testRunner)
+    androidTestImplementation (TestLibraries.espresso)
+}
