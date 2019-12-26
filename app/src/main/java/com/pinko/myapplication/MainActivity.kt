717f7fc04230eb659df767165bb81fe9fb8b3174
Aljosa <aljosa.mrak@gmail.com>
2019-12-26 22:48:12 +0100

    Fix build problems

--- app/src/main/java/com/pinko/myapplication/MainActivity.kt
+++ app/src/main/java/com/pinko/myapplication/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.pinko.myapplication
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
-import com.pinko.library.debug.DebugActivity
+//import com.pinko.library.debug.DebugActivity
 //import mu.KLogging
 
 
 //import com.krishna.debug_tools.activity.ActivityDebugTools
-class MainActivity : DebugActivity() {
+//class MainActivity : DebugActivity() {
+class MainActivity : AppCompatActivity() {
 
 //    companion object : KLogging()
 
