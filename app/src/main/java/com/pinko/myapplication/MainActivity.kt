d131a5829a8cc78596dd2b9dc307b586c3ef4b55
Aljosa <aljosa.mrak@gmail.com>
2019-12-26 22:43:53 +0100

    Fix build problems

--- app/src/main/java/com/pinko/myapplication/MainActivity.kt
+++ app/src/main/java/com/pinko/myapplication/MainActivity.kt
@@ -1,30 +1,40 @@
 package com.pinko.myapplication
 
-
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
-import mu.KLogging
+import com.pinko.library.debug.DebugActivity
+//import mu.KLogging
 
-class MainActivity : AppCompatActivity() {
+
+//import com.krishna.debug_tools.activity.ActivityDebugTools
+class MainActivity : DebugActivity() {
 
 //    companion object : KLogging()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        logger.entry()
+//        logger.entry()
+
+//        val contex: Context = applicationContext
+//        val intent = Intent(contex, ActivityDebugTools::class.java)
+//        startActivity(intent)
 
         setContent {
             MaterialTheme {
                 Greeting("Android")
             }
         }
+        super.onCreate(savedInstanceState)
     }
 }
+
+//TODO add lint checks
+
+//TODO set log levels per debug, qa, release
 
 @Composable
 private fun Greeting(name: String) {
